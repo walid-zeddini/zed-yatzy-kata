2ca8fa07e89a49ad8fea6785d9affdc0584c3c2c
Walid ZEDDINI <w.zeddini@gmail.com>
2023-11-17 23:19:32 +0100

    Main Class and Method Stubs
    Creating a main class YatzyScorer and defining method stubs for each scoring category.

---
+++ src/main/java/com/zeddini/yatzy/YatzyScorer.java
@@ -0,0 +1,130 @@
+package com.zeddini.yatzy;
+
+public class YatzyScorer {
+
+    // Method to score for Chance
+	/**
+	 * scoreChance
+	 * 
+	 * @param dice
+	 * @return
+	 */
+	public static int scoreChance(int[] dice) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+    // Method to score for Yatzy
+
+	/**
+	 * scoreYatzy
+	 * 
+	 * @param dice
+	 * @return
+	 */
+	public static int scoreYatzy(int[] dice) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	// Methods for Ones, Twos, Threes, Fours, Fives, Sixes
+	/**
+	 * scoreSingles
+	 * 
+	 * @param dice
+	 * @param number
+	 * @return
+	 */
+	public static int scoreSingles(int[] dice, int number) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	// Method for Pair
+	/**
+	 * scorePair
+	 * 
+	 * @param dice
+	 * @return
+	 */
+	public static int scorePair(int[] dice) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	// Method for Two Pairs
+	/**
+	 * scoreTwoPairs
+	 * 
+	 * @param dice
+	 * @return
+	 */
+	public static int scoreTwoPairs(int[] dice) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	// Method for Three of a Kind
+	/**
+	 * scoreThreeOfAKind
+	 * 
+	 * @param dice
+	 * @return
+	 */
+	public static int scoreThreeOfAKind(int[] dice) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	// Method for Four of a Kind
+	/**
+	 * scoreFourOfAKind
+	 * 
+	 * @param dice
+	 * @return
+	 */
+	public static int scoreFourOfAKind(int[] dice) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	// Method for Small Straight
+	/**
+	 * scoreSmallStraight
+	 * 
+	 * @param dice
+	 * @return
+	 */
+	public static int scoreSmallStraight(int[] dice) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	// Method for Large Straight
+	/**
+	 * scoreLargeStraight
+	 * 
+	 * @param dice
+	 * @return
+	 */
+	public static int scoreLargeStraight(int[] dice) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	// Method for Full House
+	/**
+	 * scoreFullHouse
+	 * 
+	 * @param dice
+	 * @return
+	 */
+	public static int scoreFullHouse(int[] dice) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	public static void main(String[] args) {
+
+	}
+}
