03f9d05d36eae5176a21f1697f3b6d40c08660e9
Walid ZEDDINI <w.zeddini@gmail.com>
2023-11-17 23:39:55 +0100

    Test Class Test for Chance Scoring
    Tests Run (Red Phase)

---
+++ src/test/java/com/zeddini/yatzy/ChanceScoringTest.java
@@ -0,0 +1,13 @@
+package com.zeddini.yatzy;
+
+import org.junit.jupiter.api.Test;
+
+public class ChanceScoringTest {
+
+    @Test
+    public void testScore() {
+        IScoringStrategy chanceScoring = new ChanceScoringImpl();
+        assertEquals(15, chanceScoring.score(new int[]{1, 2, 3, 4, 5}));
+        assertEquals(21, chanceScoring.score(new int[]{4, 5, 5, 6, 1}));
+    }
+}
